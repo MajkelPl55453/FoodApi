f54e87f487a24de570eae5ba721da12835d0c020
Michal-l\Majkel <mih.pago@op.pl>
2018-06-13 09:09:37 +0200

    no message

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -180,7 +180,7 @@ class ApiController extends Controller
 //        {
 //            return response()->json(['recipes' => []]);
 //        }
-        $data['ids'] = Filter::getString($data['ids']);
+//        $data['ids'] = Filter::getString($data['ids']);
         $data['ids'] = '309,248,3,20';
         $params = ['limit' => $limit, 'offset' => $offset, 'orderBy' => 'id', 'orderSort' => 'ASC'];
         $where['id'] = 'id IN ('.$data['ids'].')';
